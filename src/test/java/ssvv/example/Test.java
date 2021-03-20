fbd26037a1553ef790da80a8ebc6cd4b4b4e2588
raduceaca1234 <ceaca.radu@yahoo.com>
2021-03-20 15:33:20 +0200

    lab2

---
+++ src/test/java/ssvv/example/Test.java
@@ -0,0 +1,17 @@
+package ssvv.example;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class Test {
+
+    @org.junit.Test
+    public void test1(){
+        assertTrue(true);
+    }
+
+    @org.junit.Test
+    public void test2(){
+        assertFalse(false);
+    }
+}
